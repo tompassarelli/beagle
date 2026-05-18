a4bf6f712270a9141f4bdf270f133633836ab428
tompassarelli <tom.passarelli@protonmail.com>
2026-05-18 18:00:21 +0700

    bump version to 0.6.1

--- info.rkt
+++ info.rkt
@@ -6,6 +6,6 @@
 (define scribblings '(("scribblings/beagle.scrbl" ())))
 (define raco-commands
   '(("beagle" beagle/private/raco-cmd "build, check, expand" #f)))
-(define version "0.6.0")
+(define version "0.6.1")
 (define pkg-desc "Typed authoring layer that compiles to Clojure source.")
 (define pkg-authors '(tom))
