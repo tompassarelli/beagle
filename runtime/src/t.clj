5b9a347802307593c97540793cc6fe279927f6cd
tompassarelli <tom.passarelli@protonmail.com>
2026-06-15 04:15:19 +0700

    chore: drop stray runtime/src/t.clj (sanity-build output accidentally committed)

--- runtime/src/t.clj
+++
@@ -1,6 +0,0 @@
-(ns t)
-
-^{:line 5 :file "/tmp/if.bclj"} (defn f [x]
-  ^{:line 5 :file "/tmp/if.bclj"} (if ^{:line 5 :file "/tmp/if.bclj"} (> x 0) x ^{:line 5 :file "/tmp/if.bclj"} (do
-  0
-  1)))
