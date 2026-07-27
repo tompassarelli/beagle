c7d9110e9ece10462b621846ddc76550e3d4d13c
tompassarelli <tom.passarelli@protonmail.com>
2026-07-27 10:33:43 +0800

    test(rewrite): specify case-to-match migration

--- beagle-test/tests/rewrite-roundtrip.rkt
+++ beagle-test/tests/rewrite-roundtrip.rkt
@@ -14,7 +14,9 @@
          (only-in beagle/lang/reader-impl beagle-read)
          (only-in beagle/private/rewrite
                   read-beagle-source write-beagle-source
-                  define-rewrite get-rule rewrite-text rewrite-result-rewritten))
+                  define-rewrite get-rule rewrite-text
+                  rewrite-result-changed? rewrite-result-rewritten)
+         beagle/private/rewrites/case-to-match)
 
 (define (read-all str)
   (let ([in (open-input-string str)])
@@ -56,3 +58,36 @@
   ;; re-reading the rewritten text yields the same datums the reader produced
   (check-equal? (read-all (rewrite-result-rewritten res))
                 (read-all "(def v [#?@(:clj [1] :default [])]) (def f (fn [%1 %2] (+ %1 %2)))")))
+
+(test-case "case-to-match preserves keyword and string dispatch shape"
+  (define res
+    (rewrite-text
+     (get-rule 'case-to-match)
+     "(defn dispatch [x] (case x :open 1 :closed 2 0))
+      (defn mode [x] (case x \"read\" :r \"write\" :w))"))
+  (check-true (rewrite-result-changed? res))
+  (check-equal?
+   (read-all (rewrite-result-rewritten res))
+   (read-all
+    "(defn dispatch [x] (match x [:open 1] [:closed 2] [_ 0]))
+     (defn mode [x] (match x [\"read\" :r] [\"write\" :w]))")))
+
+(test-case "case-to-match turns grouped constants into an or-pattern"
+  (define res
+    (rewrite-text
+     (get-rule 'case-to-match)
+     "(case kind (:assert :link) :write (:retract :untell) :remove :unknown)"))
+  (check-equal?
+   (read-all (rewrite-result-rewritten res))
+   (read-all
+    "(match kind [(or :assert :link) :write] [(or :retract :untell) :remove] [_ :unknown])")))
+
+(test-case "case-to-match rewrites nested cases bottom-up"
+  (define res
+    (rewrite-text
+     (get-rule 'case-to-match)
+     "(case outer :a (case inner \"x\" 1 0) 2)"))
+  (check-equal?
+   (read-all (rewrite-result-rewritten res))
+   (read-all
+    "(match outer [:a (match inner [\"x\" 1] [_ 0])] [_ 2])")))
