d8675591b15d0dd5ed8c2bcac36a610854cfe71b
tompassarelli <tom.passarelli@protonmail.com>
2026-08-29 08:24:33 -0700

    Prevent readiness self-probe socket races

--- store/server.clj
+++ store/server.clj
@@ -2521,8 +2521,13 @@
     (with-open [client (Socket.)]
       (.connect client (java.net.InetSocketAddress. host port) 5000)
       (.setSoTimeout client 5000)
-      (let [accepted (.accept server)]
-        (admit-connection! accepted))
+      ;; External clients may enter the backlog before this self-client. Serve
+      ;; them, but do not mistake their successful RPC for the readiness proof.
+      (loop []
+        (let [accepted (.accept server)
+              self-client? (= (.getLocalPort client) (.getPort accepted))]
+          (admit-connection! accepted)
+          (when-not self-client? (recur))))
       (let [output (.getOutputStream client)]
         (.write output (rpc/store-rpc-encode-packet-v2! request-packet))
         (.flush output))
